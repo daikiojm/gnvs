e60eca60972d2b67de8fd6e64334969999422036
daikiojm <daiking.ca2@gmail.com>
2021-06-10 19:16:21 +0900

    chore: routes

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import fastify from 'fastify'
 
-//
-// https://www.fastify.io/docs/latest/Serverless/#vercel
-// https://vercel.com/docs/serverless-functions/supported-languages#using-typescript
-//
-
-const server = fastify({ logger: true, ignoreTrailingSlash: true })
-
-server.get('/', async (request, reply) => {
-  return { hello: 'world', request, reply }
-})
-
-server.get('/test', async (request, reply) => {
-  return { hello: 'world', request, reply }
-})
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-  await server.ready()
-  server.server.emit('request', req, res)
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { name = 'World' } = request.query
+  response.status(200).send(`Hello ${name}!`)
 }
