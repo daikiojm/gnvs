eea4172672e38a7dace9336f8161be54457f73c1
daikiojm <daiking.ca2@gmail.com>
2021-06-12 00:16:31 +0900

    fix: use axios

--- api/authenticate.ts
+++ api/authenticate.ts
@@ -1,10 +1,13 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import ky from 'ky-universal'
+import axios from 'axios'
 
 const githubOauthAccessTokenUrl = 'https://github.com/login/oauth/access_token'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const { code = '' } = request.body as { code: string }
+  if (!code) {
+    errorResponse(response)
+  }
 
   const data = new FormData()
   data.append('client_id', '')
@@ -12,8 +15,12 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   data.append('code', code)
   data.append('redirect_url', '')
 
-  const accessTokenResponse = await ky.post(githubOauthAccessTokenUrl, {
+  const accessTokenResponse = await axios.post(githubOauthAccessTokenUrl, {
     body: data,
   })
   response.status(200).send(accessTokenResponse)
+}
+
+function errorResponse(res: VercelResponse) {
+  res.status(400).send('error')
 }
