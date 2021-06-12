224455ad18a4e12c2fe76b6e76155ccd8909336e
daikiojm <daiking.ca2@gmail.com>
2021-06-12 16:30:00 +0900

    refactor: define response json type

--- api/authenticate.ts
+++ api/authenticate.ts
@@ -1,35 +1,39 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
-import axios from 'axios'
-// import FormData from 'form-data'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import axios, { AxiosResponse } from 'axios'
 
 const githubOauthAccessTokenUrl = 'https://github.com/login/oauth/access_token'
+
+type Env = {
+  GITHUB_OAUTH_CLIENT_ID: string
+  GITHUB_OAUTH_CLIENT_SECRET: string
+  GITHUB_OAUTH_REDIRECT_URI: string
+}
+
+type AuthenticateRequest = {
+  code: string
+}
+
+type AuthenticateResponse = {
+  accessToken: string
+}
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const {
     GITHUB_OAUTH_CLIENT_ID: clientId,
     GITHUB_OAUTH_CLIENT_SECRET: clientSecret,
     GITHUB_OAUTH_REDIRECT_URI: redirectUrl,
-  } = process.env as {
-    GITHUB_OAUTH_CLIENT_ID: string
-    GITHUB_OAUTH_CLIENT_SECRET: string
-    GITHUB_OAUTH_REDIRECT_URI: string
-  }
+  } = process.env as Env
 
   if (!clientId || !clientSecret || !redirectUrl) {
     serverErrorResponse(response)
   }
 
-  const { code = '' } = request.body as { code: string }
+  const { code = '' } = request.body as AuthenticateRequest
 
   if (!code) {
     requestErrorResponse(response)
   }
 
-  // const data = new FormData()
-  // data.append('client_id', clientId)
-  // data.append('client_secret', clientSecret)
-  // data.append('code', code)
-  // data.append('redirect_url', redirectUrl)
   const body = {
     client_id: clientId,
     client_secret: clientSecret,
@@ -37,16 +41,45 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     redirect_url: redirectUrl,
   }
 
-  const accessTokenResponse = await axios.post(githubOauthAccessTokenUrl, {
-    body,
-  })
-  response.status(200).send(accessTokenResponse)
+  try {
+    const accessTokenResponse = await axios.post<string>(
+      githubOauthAccessTokenUrl,
+      {
+        body,
+      }
+    )
+    successResponse<AuthenticateResponse>(
+      response,
+      buildResponse(accessTokenResponse)
+    )
+  } catch {
+    serverErrorResponse(response)
+  }
 }
 
 function serverErrorResponse(res: VercelResponse) {
-  res.status(500).send('error')
+  res.status(500).send('Server error')
 }
 
 function requestErrorResponse(res: VercelResponse) {
-  res.status(400).send('error')
+  res.status(400).send('Request error')
+}
+
+function successResponse<R = any>(res: VercelResponse, body: R) {
+  res.status(200).json(body)
+}
+
+function buildResponse(
+  accessTokenResponse: AxiosResponse<string>
+): AuthenticateResponse {
+  const { data: paramsString } = accessTokenResponse
+  const params = new URLSearchParams(paramsString)
+  const accessToken = params.get('access_token')
+  if (!accessToken) {
+    throw new Error('access_token not found')
+  }
+
+  return {
+    accessToken,
+  }
 }
