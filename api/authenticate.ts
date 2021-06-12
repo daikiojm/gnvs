a1b7a39f67ea647349df8b593e61a606e78079aa
daikiojm <daiking.ca2@gmail.com>
2021-06-12 11:56:01 +0900

    feat: load server env variables

--- api/authenticate.ts
+++ api/authenticate.ts
@@ -5,16 +5,29 @@ import FormData from 'form-data'
 const githubOauthAccessTokenUrl = 'https://github.com/login/oauth/access_token'
 
 export default async (request: VercelRequest, response: VercelResponse) => {
+  const {
+    GITHUB_OAUTH_CLIENT_ID: clientId,
+    GITHUB_OAUTH_CLIENT_SECRET: clientSecret,
+    GITHUB_OAUTH_REDIRECT_URI: redirectUrl,
+  } = process.env as {
+    GITHUB_OAUTH_CLIENT_ID: string
+    GITHUB_OAUTH_CLIENT_SECRET: string
+    GITHUB_OAUTH_REDIRECT_URI: string
+  }
+  if (!clientId || !clientSecret || !redirectUrl) {
+    serverErrorResponse(response)
+  }
+
   const { code = '' } = request.body as { code: string }
   if (!code) {
-    errorResponse(response)
+    requestErrorResponse(response)
   }
 
   const data = new FormData()
-  data.append('client_id', '')
-  data.append('client_secret', '')
+  data.append('client_id', clientId)
+  data.append('client_secret', clientSecret)
   data.append('code', code)
-  data.append('redirect_url', '')
+  data.append('redirect_url', redirectUrl)
 
   const accessTokenResponse = await axios.post(githubOauthAccessTokenUrl, {
     body: data,
@@ -22,6 +35,10 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   response.status(200).send(accessTokenResponse)
 }
 
-function errorResponse(res: VercelResponse) {
+function serverErrorResponse(res: VercelResponse) {
+  res.status(500).send('error')
+}
+
+function requestErrorResponse(res: VercelResponse) {
   res.status(400).send('error')
 }
