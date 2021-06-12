14fed1a8a7e0ced6608870346f35100d4ae57dda
daikiojm <daiking.ca2@gmail.com>
2021-06-12 15:42:32 +0900

    fix: disabled form-data

--- api/authenticate.ts
+++ api/authenticate.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 import axios from 'axios'
-import FormData from 'form-data'
+// import FormData from 'form-data'
 
 const githubOauthAccessTokenUrl = 'https://github.com/login/oauth/access_token'
 
@@ -25,14 +25,20 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     requestErrorResponse(response)
   }
 
-  const data = new FormData()
-  data.append('client_id', clientId)
-  data.append('client_secret', clientSecret)
-  data.append('code', code)
-  data.append('redirect_url', redirectUrl)
+  // const data = new FormData()
+  // data.append('client_id', clientId)
+  // data.append('client_secret', clientSecret)
+  // data.append('code', code)
+  // data.append('redirect_url', redirectUrl)
+  const body = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code,
+    redirect_url: redirectUrl,
+  }
 
   const accessTokenResponse = await axios.post(githubOauthAccessTokenUrl, {
-    body: data,
+    body,
   })
   response.status(200).send(accessTokenResponse)
 }
